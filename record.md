915b5d459c21c94202bdf335d586f762170c0a6d
ooolize <13623436954@163.com>
2024-10-18 21:43:23 +0800

    构建基础框架

---
+++ record.md
@@ -0,0 +1,6 @@
+<!--
+ * @Description: 
+ * @Author: lize
+ * @Date: 2024-10-18
+ * @LastEditors: lize
+-->
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Author: lize
+ * @Date: 2024-09-23
+ * @LastEditors: lize
+ */
+#include <fmt/core.h>
+int main() {
+  fmt::print("Hello, World!\n");
+  return 0;
+}
